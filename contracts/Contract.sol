16b8db230613afb10a3a5f90537c2b55f0f6c54a
lmatosevic <lukamatosevic5@gmail.com>
2021-07-12 22:05:25 +0200

    Updated solidity compile to 0.8.5

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5 <0.9.0;
+pragma solidity ^0.8.5;
 
 contract Contract {
   address public owner = msg.sender;
 
-  constructor() public {
+  constructor() {
   }
 }
