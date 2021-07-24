252d1da55fa5a8038562ea95e50d464d47822555
lmatosevic <lukamatosevic5@gmail.com>
2021-07-24 20:23:10 +0200

    Added test network environment

--- truffle-config.js
+++ truffle-config.js
@@ -45,8 +45,14 @@ module.exports = {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+      network_id: "5777",       // Any network (default: none)
+      websocket: true
     },
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
