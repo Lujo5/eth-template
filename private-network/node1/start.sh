24edd93df4bc4141fbdad0abb76ca0376d3579b0
lmatosevic <lukamatosevic5@gmail.com>
2021-07-25 23:40:03 +0200

    Fixed ws env variables and and added extra ws config

--- private-network/node1/start.sh
+++ private-network/node1/start.sh
@@ -7,7 +7,7 @@ bootnode_port="${BOOTNODE_PORT:-"30301"}"
 node_pnetwork_id="${NODE_NETWORK_ID:-"12345"}"
 node_port="${NODE_PORT:-"30303"}"
 node_http_port="${NODE_HTTP_PORT:-"8545"}"
-node_ws_port="${NODE_HTTP_PORT:-"8546"}"
+node_ws_port="${NODE_WS_PORT:-"8546"}"
 node_mine="${NODE_MINE:-"YES"}"
 
 mine_arg="--mine"
@@ -28,4 +28,4 @@ enode_url="enode://$enode_key@$bootnode_host:0?discport=$bootnode_port"
 
 account=$(<./data/account.txt)
 
-geth --networkid "$node_pnetwork_id" $mine_arg --datadir "./data" --port "$node_port" --ipcdisable --syncmode full --http --http.corsdomain "*" --http.port "$node_http_port" --ws --ws.port "$node_ws_port" --bootnodes "$enode_url" --allow-insecure-unlock --unlock "$account" --password "./password.txt"
+geth --networkid "$node_pnetwork_id" $mine_arg --datadir "./data" --port "$node_port" --ipcdisable --syncmode full --http --http.corsdomain "*" --http.port "$node_http_port" --ws --ws.port "$node_ws_port" --ws.origins "*" --ws.api eth,net,web3 --bootnodes "$enode_url" --allow-insecure-unlock --unlock "$account" --password "./password.txt"
