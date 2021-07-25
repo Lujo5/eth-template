e66fe0619de326078e6cd54347dbd6550f2a2a7c
lmatosevic <lukamatosevic5@gmail.com>
2021-07-25 22:10:48 +0200

    Added http and ws options for node1 and node2, increased gas limit

--- private-network/node1/start.sh
+++ private-network/node1/start.sh
@@ -6,7 +6,8 @@ bootnode_port="${BOOTNODE_PORT:-"30301"}"
 
 node_pnetwork_id="${NODE_NETWORK_ID:-"12345"}"
 node_port="${NODE_PORT:-"30303"}"
-node_rpc_port="${NODE_RPC_PORT:-"8545"}"
+node_http_port="${NODE_HTTP_PORT:-"8545"}"
+node_ws_port="${NODE_HTTP_PORT:-"8546"}"
 node_mine="${NODE_MINE:-"YES"}"
 
 mine_arg="--mine"
@@ -27,4 +28,4 @@ enode_url="enode://$enode_key@$bootnode_host:0?discport=$bootnode_port"
 
 account=$(<./data/account.txt)
 
-geth --networkid "$node_pnetwork_id" $mine_arg --datadir "./data" --port "$node_port" --ipcdisable --syncmode full --rpc --rpccorsdomain "*" --rpcport "$node_rpc_port" --allow-insecure-unlock --unlock "$account" --password "./password.txt" --bootnodes "$enode_url"
+geth --networkid "$node_pnetwork_id" $mine_arg --datadir "./data" --port "$node_port" --ipcdisable --syncmode full --http --http.corsdomain "*" --http.port "$node_http_port" --ws --ws.port "$node_ws_port" --bootnodes "$enode_url" --allow-insecure-unlock --unlock "$account" --password "./password.txt"
